59f9556639cd194c368e8853126a553758a32cf7
Rowdy Howell <rowdy@dialexa.com>
2019-05-28 10:14:38 -0500

    Updated highlighting to be more consistent.

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ const IndexPage = () => (
       </div>
       <div className={indexStyles.bio}>
         <p className={indexStyles.bioDesc}>my name is <span className={indexStyles.bioAccent}>rowdy howell.</span></p>
-        <p className={indexStyles.bioDesc}>i like to solve problems using elegant <span className={indexStyles.bioAccent}>machine learning strategies.</span></p>
+        <p className={indexStyles.bioDesc}>i like to solve problems by <span className={indexStyles.bioAccent}>designing machine learning strategies.</span></p>
         <p className={indexStyles.bioDesc}>to support these strategies i also <span className={indexStyles.bioAccent}>architect platforms.</span></p>
-        <p className={indexStyles.bioDesc}>at the end of the day i strive to foster <span className={indexStyles.bioAccent}>innovative teams</span> and <span className={indexStyles.bioAccent}>build successful products.</span></p>
+        <p className={indexStyles.bioDesc}>at the end of the day i strive to <span className={indexStyles.bioAccent}>foster innovative teams</span> and <span className={indexStyles.bioAccent}>build successful products.</span></p>
       </div>
     </div>
   </Layout>
